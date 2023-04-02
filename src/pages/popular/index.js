bc11678a5413c7288bbf15c90622425138b4e51e
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-04-02 06:37:14 -0700

    movies

--- src/pages/popular/index.js
+++ src/pages/popular/index.js
@@ -27,7 +27,7 @@ const exclude_first_movie = first_movie_exclude(movies)
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <Layout>
-       {movies[0] !== 'undefined' && <FirstMovie item={movies[0]} />}
+       {/* <FirstMovie item={movies[0]} /> */}
         <div className='container mx-auto'>
         <h1 className='my-6 text-4xl font-bold text-gray-600'>Highest Ranked Movies</h1>
         <div className='flex flex-wrap gap-4 justify-between'>
--- src/pages/top-movies/index.js
+++ src/pages/top-movies/index.js
@@ -28,7 +28,7 @@ const TopMovies = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <Layout>
-        {movies[0] !== 'undefined' && <FirstMovie item={movies[0]} />}
+        {/* <FirstMovie item={movies[0]} /> */}
         <div className='container mx-auto'>
         <h1 className='my-6 text-4xl font-bold text-gray-600'>Highest Ranked Movies</h1>
         <div className='flex flex-wrap gap-4 justify-between'>
