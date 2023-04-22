b2c62e9ac013ab61674d6406de41ab61303755b0
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-04-22 05:01:11 -0700

    submit click

--- src/pages/reset/index.js
+++ src/pages/reset/index.js
@@ -12,6 +12,7 @@ const ResetPassword = () => {
     <div>
         <form onSubmit={handleSubmit}>
             <input className='m-2 p-2 border-2 border-gray-200 rounded-lg' type="text" value={text} onChange={(e) => setText(e.target.value)} />
+            <input type="submit" value="Submit" className='px-6 py-2 bg-indigo-600 rounded-lg font-bold text-white' onClick={handleSubmit} />
         </form>
         {JSON.stringify(router, 2, null)}
     </div>
