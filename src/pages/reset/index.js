54dd26d2511ce371cd1b5a07147908a3b4cf21d3
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-04-22 04:57:04 -0700

    changes

---
+++ src/pages/reset/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+
+const ResetPassword = () => {
+    const [text, setText] = useState('')
+    const router = useRouter()
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        console.log(text)
+    }
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input className='m-2 p-2 border-2 border-gray-200 rounded-lg' type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        </form>
+        {JSON.stringify(router, 2, null)}
+    </div>
+  )
+}
+
+export default ResetPassword
